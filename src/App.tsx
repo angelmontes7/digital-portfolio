fd0525161edac120d2436fac46d2ac021f1e43dc
Angel <angelmontes7@yahoo.com>
2025-07-22 14:26:12 -0500

    removed playbook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { Link, Routes, Route } from 'react-router-dom';
 import Resume from './pages/Resume';
-import Playbook from './pages/Playbook';
 import Contact from './pages/Contact';
 import Work from './pages/Work';
 import Home from './pages/Home';
@@ -13,7 +12,6 @@ function App() {
       <nav className="flex space-x-10 bg-black p-6 justify-center">
         <Link to="/" className="text-gray-400 hover:text-gray-100">Home</Link>
         <Link to="/resume" className="text-gray-400 hover:text-gray-100">Resume</Link>
-        <Link to="/playbook" className="text-gray-400 hover:text-gray-100">Playbook</Link>
         <Link to="/contact" className="text-gray-400 hover:text-gray-100">Contact</Link>
         <Link to="/work" className="text-gray-400 hover:text-gray-100">Work</Link>
       </nav>
@@ -21,7 +19,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/resume" element={<Resume />} />
-        <Route path="/playbook" element={<Playbook />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/work" element={<Work />} />
       </Routes>
--- src/pages/Playbook.tsx
+++
@@ -1,5 +0,0 @@
-const Playbook = () => {
-
-}
-
-export default Playbook;
